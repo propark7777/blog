97b2b7b253d31fef22b14c0343f65ef539dcdcbd
propark0709 <propark0709@gmail.com>
2022-10-08 00:34:07 +0900

    CRUD test

--- src/main/java/com/park/blog/repository/PlanRepositoryV1.java
+++ src/main/java/com/park/blog/repository/PlanRepositoryV1.java
@@ -7,6 +7,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,12 +32,10 @@ public class PlanRepositoryV1 {
 
         try {
             conn = getConnection();
-            log.info("logInfo ****************");
             pstmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
             pstmt.setString(1,plan.getExerciseName());
             pstmt.setInt(2,plan.getExerciseSetNumber());
             pstmt.setInt(3,plan.getExerciseRepetition());
-            log.info("세트 갯수: {}",plan.getExerciseSetNumber());
             pstmt.executeUpdate();
             rs = pstmt.getGeneratedKeys();
             if (rs.next()) {
@@ -52,10 +53,115 @@ public class PlanRepositoryV1 {
         }
     }
 
+    public List<Plan> findAllPlan() {
+        String sql = "select * from plan";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<Plan> plans = new ArrayList<>();
+
+            while (rs.next()) {
+                Plan plan = new Plan();
+                plan.setExerciseName(rs.getString("exercise_name"));
+                plan.setExerciseSetNumber(rs.getInt("exercise_set_number"));
+                plan.setExerciseRepetition(rs.getInt("exercise_repetition"));
+                plans.add(plan);
+            }
+            return plans;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }finally {
+            close(conn,pstmt,rs);
+        }
+    }
+
+    public Plan findPlanByExerciseId(Long exerciseId) throws SQLException {
+        String sql = "select * from plan where exercise_id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1,exerciseId);
+
+            rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                Plan plan = new Plan();
+                plan.setExerciseId(rs.getLong("exercise_id"));
+                plan.setExerciseName(rs.getString("exercise_name"));
+                plan.setExerciseSetNumber(rs.getInt("exercise_set_number"));
+                plan.setExerciseRepetition(rs.getInt("exercise_repetition"));
+                return plan;
+            }else {
+                throw new NoSuchElementException("exercise not found!!");
+            }
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(conn,pstmt,rs);
+        }
+
+    }
+
+    public void updatePlan(Long exerciseId, Plan plan) throws SQLException {
+        String sql = "update plan set exercise_name=? , exercise_set_number=? ," +
+            "exercise_repetition=? where exercise_id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,plan.getExerciseName());
+            pstmt.setInt(2,plan.getExerciseSetNumber());
+            pstmt.setInt(3,plan.getExerciseRepetition());
+            pstmt.setLong(4,exerciseId);
+
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}",resultSize);
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(conn,pstmt,null);
+        }
+
+    }
+
+    public void deletePlan(Long exerciseId) throws SQLException {
+        String sql = "delete plan where exercise_id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1,exerciseId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(conn,pstmt,null);
+        }
+
+    }
+
     private void close(Connection conn, Statement stmt, ResultSet rs) {
         JdbcUtils.closeResultSet(rs);
         JdbcUtils.closeStatement(stmt);
-        JdbcUtils.closeConnection(conn);
+        DataSourceUtils.releaseConnection(conn,dataSource);
+//        JdbcUtils.closeConnection(conn);
     }
 
     private Connection getConnection() throws SQLException {
--- src/test/java/com/park/blog/repository/PlanRepositoryV1Test.java
+++ src/test/java/com/park/blog/repository/PlanRepositoryV1Test.java
@@ -3,13 +3,19 @@ package com.park.blog.repository;
 import static com.park.blog.connection.ConnectionConst.PASSWORD;
 import static com.park.blog.connection.ConnectionConst.URL;
 import static com.park.blog.connection.ConnectionConst.USERNAME;
+import static org.assertj.core.api.Assertions.*;
 
 import com.park.blog.domain.plan.Plan;
 import com.zaxxer.hikari.HikariDataSource;
 import java.sql.SQLException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class PlanRepositoryV1Test {
 
     PlanRepositoryV1 repository;
@@ -28,8 +34,46 @@ class PlanRepositoryV1Test {
     }
 
     @Test
+    @DisplayName("운동 입력")
     void CRUD() throws SQLException {
         Plan plan = new Plan("push-up",3,10);
         repository.savePlan(plan);
+    }
+
+    @Test
+    @DisplayName("전체 운동목록 가져오기")
+    void findAll() {
+        List<Plan> plans = repository.findAllPlan();
+        log.info("plan size = {}",plans.size());
+        assertThat(plans).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("운동 업데이트")
+    void update() throws SQLException {
+        //given
+        Plan plan = new Plan("런지",10,10);
+        plan.setExerciseId(100L);
+        Plan savePlan= repository.savePlan(plan);
+        log.info("{}",plan.toString());
+        //when
+        Plan updatePlan = new Plan("윗몸 일으키기",5,5);
+        repository.updatePlan(savePlan.getExerciseId(),updatePlan);
+
+        //then
+        Plan findPlan = repository.findPlanByExerciseId(savePlan.getExerciseId());
+        assertThat(findPlan.getExerciseName()).isEqualTo(updatePlan.getExerciseName());
+        assertThat(findPlan.getExerciseSetNumber()).isEqualTo(updatePlan.getExerciseSetNumber());
+        assertThat(findPlan.getExerciseRepetition()).isEqualTo(updatePlan.getExerciseRepetition());
+    }
+
+    @Test
+    @DisplayName("운동계획 지우기")
+    void delete() throws SQLException {
+        Plan findPlan = repository.findPlanByExerciseId(37L);
+        log.info("{}",findPlan.toString());
+        repository.deletePlan(findPlan.getExerciseId());
+
+        assertThat(repository.findAllPlan().size()).isEqualTo(0);
     }
 }
